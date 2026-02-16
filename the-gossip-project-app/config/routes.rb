441c7d42102900a8f2fea28bff9031f1f7d00f72
crousty24-bit <allen.kch24@gmail.com>
2026-02-16 16:59:20 +0100

    add index

--- the-gossip-project-app/config/routes.rb
+++ the-gossip-project-app/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  root "home#index"
+
+  get "dynamic_pages/welcome"
   get "up" => "rails/health#show", as: :rails_health_check
   get '/static_pages/contact', to: 'static_pages#contact'
+  get '/static_pages/team', to: 'static_pages#team'
 end

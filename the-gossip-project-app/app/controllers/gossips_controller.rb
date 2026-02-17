c0f6737826a2c323d69f3c146f097bfec1db4b4a
crousty24-bit <allen.kch24@gmail.com>
2026-02-17 18:05:35 +0100

    Refactor GossipsController and update routes for improved functionality and organization

--- the-gossip-project-app/app/controllers/gossips_controller.rb
+++ the-gossip-project-app/app/controllers/gossips_controller.rb
@@ -1,5 +1,19 @@
 class GossipsController < ApplicationController
   def show
-  @gossip = Gossip.find(params[:id])
+    @gossip = Gossip.find(params[:id])
+  end
+  def new
+  end
+  def create
+    @gossip = Gossip.new(gossip_params)# 1) On récupère les données envoyées par le formulaire
+    if @gossip.save# 2) On tente de sauvegarder en base de données
+      redirect_to gossip_path(@gossip), notice: "Potin créé avec succès !"# 3) Si succès → redirection vers la page show du potin créé
+    else
+      render :new # 4) Si échec → on réaffiche le formulaire avec les erreurs
+    end
+  end
+  private
+  def gossip_params # Strong parameters : sécurité Rails
+    params.require(:gossip).permit(:title, :content)
   end
 end
--- the-gossip-project-app/config/routes.rb
+++ the-gossip-project-app/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
+  get "up" => "rails/health#show", as: :rails_health_check
 
   get "/users/:id", to: "users#show", as:"user"
   get "welcome/:first_name", to: "dynamic_pages#welcome"
-  get "up" => "rails/health#show", as: :rails_health_check
   get '/static_pages/contact', to: 'static_pages#contact'
   get '/static_pages/team', to: 'static_pages#team'
-  get 'gossips', to: 'gossips#index', as: :gossips
-  get 'gossips/:id', to: 'gossips#show', as: :gossip
+  resources :gossips
 end

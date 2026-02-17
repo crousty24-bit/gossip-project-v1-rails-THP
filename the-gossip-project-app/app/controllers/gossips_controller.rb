4932f0e71284d7e51afc3f9a87adebfb4c43f7dc
Meetheora <xenotimedev@gmail.com>
2026-02-17 16:41:52 +0100

    Remove index action from GossipsController and update show view link to redirect to root path

--- the-gossip-project-app/app/controllers/gossips_controller.rb
+++ the-gossip-project-app/app/controllers/gossips_controller.rb
@@ -1,10 +1,4 @@
 class GossipsController < ApplicationController
-  def index
-    @gossips = Gossip.all
-    render 'home/index'
-  end
-
-  
   def show
   @gossip = Gossip.find(params[:id])
   end

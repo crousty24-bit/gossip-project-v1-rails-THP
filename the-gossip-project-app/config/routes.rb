990c27802595d4657d6be56de3546962ccdb254d
crousty24-bit <allen.kch24@gmail.com>
2026-02-18 14:35:43 +0100

    refacto: add routes resources for show users

--- the-gossip-project-app/config/routes.rb
+++ the-gossip-project-app/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
-
-  get "/users/:id", to: "users#show", as:"user"
+  
   get "welcome/:first_name", to: "dynamic_pages#welcome"
+  resources :users, only: [:show]
   resources :teams, only: [:index]
   resources :contacts, only: [:new, :create]
   resources :gossips
